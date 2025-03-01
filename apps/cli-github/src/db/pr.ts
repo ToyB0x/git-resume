44269f826c23578c64a8cf163056b1ff05eaf319
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-03-01 11:43:33 +0900

    update table

--- apps/cli-github/src/db/pr.ts
+++ apps/cli-github/src/db/pr.ts
@@ -1,21 +1,14 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
-import { repositoryTbl } from "./repository";
 
 export const prTbl = sqliteTable("pr", {
   id: int().primaryKey(), // github pr id
-  title: text("title"),
   number: int("number").notNull(), // github pr number (eg: #123)
-  additions: int("additions").notNull(),
-  deletions: int("deletions").notNull(),
-  changedFiles: int("changed_files").notNull(),
-  mergedAt: int("merged_at", { mode: "timestamp_ms" }),
+  repositoryUrl: text("repository_url").notNull(), // github issue or pr number (eg: #123)
+  title: text("title").notNull(),
+  body: text("body"),
+  authorId: int("author_id").notNull(),
   createdAt: int({ mode: "timestamp_ms" }).notNull(),
   updatedAt: int({ mode: "timestamp_ms" }).notNull(),
-  authorId: int("author_id").notNull(),
-  repositoryId: int("repository_id")
-    .notNull()
-    .references(() => repositoryTbl.id, {
-      onUpdate: "cascade",
-      onDelete: "cascade",
-    }),
+  closedAt: int({ mode: "timestamp_ms" }),
+  diff: text("diff").notNull(),
 });
