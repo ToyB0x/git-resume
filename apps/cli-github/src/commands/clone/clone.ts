c9e6f5da392b1191b128262e9bbc0b0153edaf77
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-03-07 10:13:46 +0900

    feat: add clone command to CLI and update cloneOrPullRepositories method signature

---
+++ apps/cli-github/src/commands/clone/clone.ts
@@ -0,0 +1,30 @@
+import { gitService } from "@/services/git/interfaces";
+import { gitHubService } from "@/services/github/interfaces";
+import { logger } from "@/utils";
+import { PromisePool } from "@supercharge/promise-pool";
+
+export const clone = async (userName: string, withGhCommand: boolean) => {
+  const repositories =
+    await gitHubService.getUserCommitedRepositories(userName);
+
+  const { errors } = await PromisePool.for(repositories)
+    .withConcurrency(10)
+    .process(async (repo, i) => {
+      logger.info(
+        `Start aggregate:repository ${repo.owner}/${repo.name} (${i + 1}/${repositories.length})`,
+      );
+
+      if (withGhCommand) {
+        await gitHubService.cloneOrPullRepositories(repo);
+      } else {
+        await gitService.cloneOrPullRepository(repo);
+      }
+    });
+
+  if (errors.length) {
+    logger.error(`errors occurred: ${errors.length}`);
+    for (const error of errors) {
+      logger.error(JSON.stringify(error));
+    }
+  }
+};
