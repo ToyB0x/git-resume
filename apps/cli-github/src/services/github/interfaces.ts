575aad64f88dc5047634c2b3df5242266acc3fe9
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-03-07 09:17:39 +0900

    refactor: add clone and update methods to GithubService interface

--- apps/cli-github/src/services/github/interfaces.ts
+++ apps/cli-github/src/services/github/interfaces.ts
@@ -5,9 +5,18 @@ import { getUserDetail } from "./getUserDetail";
 interface GithubService {
   getUserDetail: (userName: string) => Promise<User>;
   getUserCommitedRepositories: (userName: string) => Promise<Repository[]>;
+  // instead of git commands (for case not setup git ssh config, like in CI)
+  cloneRepositories: () => Promise<void>;
+  updateRepositories: () => Promise<void>;
 }
 
 export const gitHubService: GithubService = {
   getUserDetail: getUserDetail,
   getUserCommitedRepositories: getUserCommitedRepositories,
+  cloneRepositories: async () => {
+    throw Error("not implemented");
+  },
+  updateRepositories: async () => {
+    throw Error("not implemented");
+  },
 };
