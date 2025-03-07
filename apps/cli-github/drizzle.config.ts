890100a7d994427f53fa33cd5e0673c6d652a517
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-03-07 09:48:34 +0900

    chore: remove unused dependencies from package.json and pnpm-lock.yaml (drizzle)

--- apps/cli-github/drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  schema: "./src/services/db/schemas/index.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: "sqlite/github.db",
-  },
-});
