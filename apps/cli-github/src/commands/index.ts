15598ba81f2a60256a9768c595fadb92d246e29a
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-03-07 10:42:21 +0900

    feat: remove aggregate and pack commands from CLI

--- apps/cli-github/src/commands/index.ts
+++ apps/cli-github/src/commands/index.ts
@@ -1,14 +1,10 @@
 import { Command } from "commander";
-import { newAggregateCommand } from "./aggregate";
 import { newCloneCommand } from "./clone";
 import { newGenerateCommand } from "./generate";
-import { newPackCommand } from "./pack";
 import { newSummaryCommand } from "./summary";
 
 const main = async () => {
   const program = new Command();
-  program.addCommand(newAggregateCommand());
-  program.addCommand(newPackCommand());
   program.addCommand(newGenerateCommand());
   program.addCommand(newCloneCommand());
   program.addCommand(newSummaryCommand());
