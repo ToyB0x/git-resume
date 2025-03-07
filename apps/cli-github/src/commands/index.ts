a2c1d6614d9ae4ed2437b42c378909f365d0d793
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-03-07 09:43:51 +0900

    feat: implement command-line interface in index and update job script path

--- apps/cli-github/src/commands/index.ts
+++ apps/cli-github/src/commands/index.ts
@@ -1,3 +1,20 @@
-export * from "./aggregate";
-export * from "./generate";
-export * from "./pack";
+import { Command } from "commander";
+import { newAggregateCommand } from "./aggregate";
+import { newGenerateCommand } from "./generate";
+import { newPackCommand } from "./pack";
+
+const main = async () => {
+  const program = new Command();
+  program.addCommand(newAggregateCommand());
+  program.addCommand(newPackCommand());
+  program.addCommand(newGenerateCommand());
+
+  try {
+    await program.parseAsync();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+main();
