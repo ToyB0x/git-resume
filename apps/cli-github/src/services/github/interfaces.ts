9fa4a53b1692a84baf136598940dea52f9fd82de
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-03-09 10:47:53 +0900

    fix: lint typecheck

--- apps/cli-github/src/services/github/interfaces.ts
+++ apps/cli-github/src/services/github/interfaces.ts
@@ -6,14 +6,14 @@ interface GithubService {
   getUserDetail: (userName: string) => Promise<User>;
   getUserCommitedRepositories: (userName: string) => Promise<Repository[]>;
   // instead of git commands (for case not setup git ssh config, like in CI)
-  // cloneOrPullRepositories: (repository: Repository) => Promise<void>;
+  cloneOrPullRepositories: (repository: Repository) => Promise<void>;
 }
 
 export const gitHubService: GithubService = {
   getUserDetail: getUserDetail,
   getUserCommitedRepositories: getUserCommitedRepositories,
   // instead of git commands (for case not setup git ssh config, like in CI)
-  // cloneOrPullRepositories: async (repository: Repository) => {
-  //   throw Error("not implemented: " + repository.name);
-  // },
+  cloneOrPullRepositories: async (repository: Repository) => {
+    throw Error(`not·implemented:·${repository.name}`);
+  },
 };
