66f0981e1620bfff73ea77fc2714d138e58fbdfb
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-03-30 17:42:15 +0900

    feat: update database configuration to use environment variable and enhance migration script

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -4,11 +4,6 @@ export default defineConfig({
   schema: "./src/index.ts",
   dialect: "postgresql",
   dbCredentials: {
-    host: "localhost",
-    port: 5432,
-    user: "user",
-    password: "pass",
-    database: "resume",
-    // ssl: true, // can be boolean | "require" | "allow" | "prefer" | "verify-full" | options from node:tls
+    url: process.env.RESUME_DB,
   },
 });
