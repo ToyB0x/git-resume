5523ac58594c5767b2267aaa05e2366b54796612
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-03-30 18:49:03 +0900

    feat: remove GITHUB_TOKEN from env schema and sample file for public mode

--- apps/job/src/utils/env.ts
+++ apps/job/src/utils/env.ts
@@ -1,8 +1,6 @@
 import * as v from "valibot";
 
 const envSchema = v.object({
-  // RESUME_DIR: v.pipe(v.string(), v.minLength(1)), // 現在は未使用
-  RESUME_USERNAME: v.pipe(v.string(), v.minLength(3)),
   RESUME_GEMINI_API_KEY: v.pipe(v.string(), v.minLength(10)),
   RESUME_ENV: v.union([
     v.literal("test"),
@@ -11,7 +9,8 @@ const envSchema = v.object({
     v.literal("stg"),
     v.literal("prd"),
   ]),
-  GITHUB_TOKEN: v.pipe(v.string(), v.minLength(5)),
+  // public mode では不要
+  // GITHUB_TOKEN: v.pipe(v.string(), v.minLength(5)),
 });
 
 export const env = v.parse(envSchema, process.env);
