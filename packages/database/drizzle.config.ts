a3202bf01599964d2ebc98d283b498f9b3e25c15
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-03-31 00:24:18 +0900

    feat: add environment variable check for database URL and update dependencies

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "drizzle-kit";
 
+// biome-ignore lint/complexity/useLiteralKeys: ts warning conflict
+const dbUrl = process.env["RESUME_DB"];
+if (!dbUrl) {
+  throw Error("RESUME_DB environment variable is not set");
+}
+
 export default defineConfig({
   schema: "./src/index.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.RESUME_DB,
+    url: dbUrl,
   },
 });
