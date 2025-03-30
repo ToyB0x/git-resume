d9419b0e4eb300a9b7f896bd86edb444c3b7717e
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-03-31 00:49:15 +0900

    chore: update region from asia-northeast1 to us-central1  gcp (#93)
    * chore: update region from asia-northeast1 to us-central1 in cloudbuild and terraform files
    
    * feat: add gemini secret and update IAM permissions for secret access

--- infra/build.tf
+++ infra/build.tf
@@ -2,7 +2,7 @@ resource "google_cloudbuild_trigger" "git_job" {
   count = var.project_id == "git-resume-local" ? 0 : 1 # ローカル環境向けではデプロイしない
 
   name     = "git-job"
-  location = "asia-northeast1"
+  location = "us-central1"
   filename = "apps/job/cloudbuild.yaml"
 
   github {
