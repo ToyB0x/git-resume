d6bdb69a15870ff02ed25d9aa5109e847701c700
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-03-30 21:24:17 +0900

    feat: add logging for pack cloning progress in summarize function

--- apps/job/src/commands/run/steps/summarize.ts
+++ apps/job/src/commands/run/steps/summarize.ts
@@ -1,10 +1,14 @@
-import { env } from "@/utils";
+import { env, logger } from "@/utils";
 import { packService, summaryService } from "@resume/services";
 import { retry } from "@resume/utils";
 
 export const summarize = async (userName: string) => {
   const packs = packService.load(userName);
-  for (const pack of packs) {
+  for (const [i, pack] of Object.entries(packs)) {
+    logger.info(
+      `Cloning ${pack.meta.owner}/${pack.meta.repo} (${i + 1} / ${packs.length})`,
+    );
+
     await retry({
       fn: async () =>
         await summaryService.create(userName, pack, env.RESUME_GEMINI_API_KEY),
