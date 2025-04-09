3407e91a9e1e29fe8082f8e2d603014da0bda6e0
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-04-09 23:06:19 +0900

    feat: simplify route handlers and update TypeScript configuration

--- apps/api/src/routes/api/github/index.ts
+++ apps/api/src/routes/api/github/index.ts
@@ -1,9 +1,7 @@
 import { Hono } from "hono";
 import { analysisHandler } from "./analysis";
 import { getUserHandler } from "./getUser";
-import { getUserSseHandler } from "./getUserSse";
 
 export const githubRoute = new Hono()
   .get("analysis/:userName", ...analysisHandler)
-  .get(":userName", ...getUserHandler)
-  .get(":userName/progress", ...getUserSseHandler);
+  .get(":userName", ...getUserHandler);
