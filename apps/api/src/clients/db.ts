48f7c868d2306e16cb7c946d931cc194d4ce14d2
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-04-09 23:11:12 +0900

    fix: update import path for utils in db.ts

--- apps/api/src/clients/db.ts
+++ apps/api/src/clients/db.ts
@@ -1,6 +1,6 @@
-import { env } from "@/utils";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import { env } from "../utils";
 
 const sql = neon(env.RESUME_DB);
 export const db = drizzle({ client: sql });
